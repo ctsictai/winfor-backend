0616884a07724b96a312331e985f26f01fe72d4e
kykevin1219 <kykevin1219@gmail.com>
2019-10-28 22:21:27 +0900

    기본 세팅 수정 및 AccountEndpoint 작업 진행중

--- account/models.py
+++ account/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 
 # Create your models here.
-class Account:
-    email = models.CharField(max_length = 100, Unique = True)
+class Account(models.Model):
+    email = models.CharField(max_length = 100, unique = True)
     password= models.CharField(max_length = 200)
-    created_at = models.DateTimeField(auto_add_now = True)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+    class Meta:
+        db_table = "account"
