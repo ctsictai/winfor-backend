938064679e1a688c739962474217df432517e9a0
ctsictai <ctsictai@gmail.com>
2019-11-01 13:34:06 +0900

    championsupdate

--- crawler/cham_crawler.py
+++ crawler/cham_crawler.py
@@ -1,6 +1,8 @@
 from selenium  import webdriver
 import time
 import os
+import sys
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'winforgg.settings')
 import django
 django.setup()
