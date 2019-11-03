ef15f23de2973f8e45971546bf798fc3b6de51f2
kykevin1219 <kykevin1219@gmail.com>
2019-11-03 21:08:09 +0900

    adding summonerid - models

--- account/models.py
+++ account/models.py
@@ -2,9 +2,12 @@ from django.db import models
 
 # Create your models here.
 class Account(models.Model):
-    email = models.CharField(max_length = 100, unique = True)
-    password= models.CharField(max_length = 200)
-    created_at = models.DateTimeField(auto_now_add = True)
-    updated_at = models.DateTimeField(auto_now = True)
+    email =         models.CharField(max_length = 100, unique = True)
+    password=       models.CharField(max_length = 200)
+    summoner_name = models.CharField(max_length = 50, unique = True)    #소환사명 추가
+    summoner_id =   models.CharField(max_length = 100, unique = True)   #소환사 ID(riot API)
+    created_at =    models.DateTimeField(auto_now_add = True)
+    updated_at =    models.DateTimeField(auto_now = True)
+
     class Meta:
         db_table = "accounts"
