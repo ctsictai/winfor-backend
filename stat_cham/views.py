c7b5ab7e0fb05c77de855db9404216c0e7625ab5
ctsictai <ctsictai@gmail.com>
2019-11-03 15:49:52 +0900

    11031549stat_cham/views.py update

--- stat_cham/views.py
+++ stat_cham/views.py
@@ -3,27 +3,25 @@ from django.http       import JsonResponse
 from django.views      import View
 from .models           import Stat_champions
 from champions.models  import Champions
-from django.core.exceptions import ObjectDoesNotExist
 
 class StatChampionsView(View):
     def get(self, request):
-            stat_list = [{
-                "champions_id"      : stat['champions_id'],
-                "id"                : stat['id'],
-                "rank"              : stat['rank'],
-                "winRate"           : stat['win_rates'],
-                "playCount"         : stat['player_numbers'],
-                "averageScore"      : stat['kda'],
-                "csScore"           : stat['cs_avg'],
-                "goldScore"         : stat['gold_avg'],
-                "championImgSrc"    : [{
-                    "champion_img_src" : champions_id['champion_img_src']
-                    } for champions_id in Champions.objects.filter(id=stat['champions_id']).values()],
-                "championName"      : [{
-                    "champion_name" : champions_id['champion_name']
-                    } for champions_id in Champions.objects.filter(id=stat["champions_id"]).values()],
-                } for stat in Stat_champions.objects.all().values()
-                ]
+        stat_list = [{
+            "id"                : stat['id'],
+            "rank"              : stat['rank'],
+            "winRate"           : stat['win_rates'],
+            "playCount"         : stat['player_numbers'],
+            "averageScore"      : stat['kda'],
+            "csScore"           : stat['cs_average'],
+            "goldScore"         : stat['gold_average'],
+            "championImgSrc"    : [{
+                "champion_img_src" : champions_id['champion_img_src']
+                } for champions_id in Champions.objects.filter(id=stat['champions_id']).values()],
+            "championName"      : [{
+                "champion_name" : champions_id['champion_name']
+                } for champions_id in Champions.objects.filter(id=stat["champions_id"]).values()],
+            } for stat in Stat_champions.objects.all().values()
+            ]
 
-            return JsonResponse({"stat_cham_data" : stat_list}, status=200)
+        return JsonResponse({"stat_cham_data" : stat_list}, status=200)
 
