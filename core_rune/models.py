251cbb9ccc95ed2b7a4e0e5f98a3916e2bda45cb
kykevin1219 <kykevin1219@gmail.com>
2019-11-05 13:47:45 +0900

    URL to URLFields

--- core_rune/models.py
+++ core_rune/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Core_rune(models.Model):
     rune_name =        models.CharField(max_length = 20, unique = True) #핵심룬 이름
-    rune_img_src =     models.CharField(max_length = 500)               #핵심룬 이미지경로
+    rune_img_src =     models.URLField(max_length = 3000)               #핵심룬 이미지경로
     riot_rune_id =     models.IntegerField()                            #riot API 룬id
     rune_description = models.CharField(max_length = 300)               #핵심룬 설명
 
